a03763db2444ed83c055f253b38abf3171e5ed36
Shreyash - Shukla <s.shreyashxp@gmail.com>
2025-01-19 00:36:49 +0530

    Conncected DataBase, setup auth controller, and env file for database authentication

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+export const signup = (req, res) => {
+    res.send("signup route");
+};
+
+
+export const login = (req, res) => {
+    res.send("login route");
+};
+
+
+export const logout = (req, res) => {
+    res.send("logout route");
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
 
-
-
+import authRoutes from "./routes/auth.route.js";
+dotenv.config();
 const app = express();
 
-app.listen(5001,() =>{
-    console.log("Server is running on port 5001");
+const PORT = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    console.log("Server is running on PORT:" + PORT);
 });
