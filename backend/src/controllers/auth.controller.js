72bd57d867e6444e05fa02bf65a97aab13f9c3c6
Shreyash - Shukla <s.shreyashxp@gmail.com>
2025-01-19 01:17:16 +0530

    created model for signup model and checking for signup and passsword

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,28 @@
-export const signup = (req, res) => {
-    res.send("signup route");
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup =async (req, res) => {
+    const {fullName,email,password} = req.body
+   try {
+     if(password.length < 6){
+        return res.status(400).json ({message: "Password must be atleast 6 character"});
+     }
+
+     const user = await User.findOne({email});
+     if(user) return res.status(400).json ({message: "Email already Exists"});
+
+     const salt = await bcrypt.genSalt(10)
+     const hashedPassword = await bcrypt.hash(password,salt);
+
+     
+
+
+
+
+
+   } catch (error) {
+    
+   }
 };
 
 
