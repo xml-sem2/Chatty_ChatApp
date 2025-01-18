9bef428e6759dbfc45cbee02dfc8a54e6f5fcb2b
Shreyash - Shukla <s.shreyashxp@gmail.com>
2025-01-19 00:52:43 +0530

    Created user model for keeping info about user name email profilepic {ITS SCHEMA}

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import {connectDB} from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 dotenv.config();
@@ -11,4 +12,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on PORT:" + PORT);
+    connectDB();
 });
