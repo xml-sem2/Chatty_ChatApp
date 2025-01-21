907bde279468b487bf700b48f2b5925a8e388387
Shreyash - Shukla <s.shreyashxp@gmail.com>
2025-01-21 20:13:20 +0530

    completion of signup module
    it will make new entry of new user in the database

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,14 @@
+import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
 export const signup =async (req, res) => {
     const {fullName,email,password} = req.body
    try {
+    if(!fullName || !email || !password){
+        return res.status(400).json ({message: "All fields are required"});
+    }
+    
      if(password.length < 6){
         return res.status(400).json ({message: "Password must be atleast 6 character"});
      }
@@ -11,17 +16,38 @@ export const signup =async (req, res) => {
      const user = await User.findOne({email});
      if(user) return res.status(400).json ({message: "Email already Exists"});
 
-     const salt = await bcrypt.genSalt(10)
+     const salt = await bcrypt.genSalt(10);
+
      const hashedPassword = await bcrypt.hash(password,salt);
 
-     
 
+     const newUser = new User({
+        fullName,
+        email,
+        password: hashedPassword
+     })
 
+     if(newUser){
+        // generate jwt(json web token) here
+        generateToken(newUser._id,res)
+        await newUser.save();
 
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            email: newUser.email,
+            profilePic: newUser.profilePic,
+
+     });
+
+     }else{
+        res.status(400).json({message: "Invalid user data"});
+     }
 
 
    } catch (error) {
-    
+    console.log("Error in signup controller",error.message);
+    res.status(500).json({message: "Internal Server Error"});
    }
 };
 
---
+++ backend/src/lib/utils.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken"
+
+export const generateToken = (userId,res) =>{
+    
+    const token = jwt.sign({userId},process.env.JWT_SECRET,{
+        expiresIn: "7d"
+    })
+
+    res.cookie("jwt",token,{
+        maxAge: 7 * 24 * 60 * 60 * 1000, // millisecond
+        httpOnly: true, // prevent attacks
+        sameSite: "strict", // prevent attacks
+        secure: process.env.NODE_ENV !== "development",
+    });
+    return token;
+};
