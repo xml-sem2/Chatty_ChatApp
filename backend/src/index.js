03982f9f034007bc2ca966fbec2ba55a0d66b2ca
Rahul PD <rahulx0202@gmail.com>
2025-01-24 03:08:39 +0530

    created message model and routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,10 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser"
 
 import {connectDB} from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 dotenv.config();
 const app = express();
 
@@ -16,6 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 //hi
 
