526fb98c44837b2db8f960bd950a7b2eb31555e2
Veera <136969041+Venuka26@users.noreply.github.com>
2025-03-06 11:20:34 +0530

    Update login page with authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -38,7 +38,7 @@ const App = () => {
           <Route path="/signup" element={!authUser ? <SignUpPage /> :<Navigate to = "/"/>} />
           <Route path="/login" element={!authUser ?<LoginPage /> : <Navigate to ="/"/>} />
           <Route path="/settings" element={<SettingsPage /> } />
-          <Route path="/profle" element={authUser ? <ProfilePage />: <Navigate to ="/login"/>} />
+          <Route path="/profile" element={authUser ? <ProfilePage />: <Navigate to ="/login"/>} />
       </Routes>
 
      <Toaster/>
